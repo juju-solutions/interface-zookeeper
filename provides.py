6a860c2d6fdf65369db3a9507e0a34394a0d6bed
Konstantinos Tsakalozos <konstantinos.tsakalozos@canonical.com>
2016-03-15 18:14:19 +0200

    Adding rest port in the interface

--- provides.py
+++ provides.py
@@ -33,8 +33,9 @@ class ZookeeperProvides(RelationBase):
         self.remove_state('{relation_name}.ready')
         self.remove_state('{relation_name}.joined')
 
-    def send_port(self, port):
+    def send_port(self, port, rest_port):
         conv = self.conversation()
         conv.set_remote(data={
             'port': port,
+            'rest_port': rest_port,
         })
